d8ccb596704cbd97a30ad8a99e35ecbeda0bda62
xvazquezc <xvazquezc@users.noreply.github.com>
2018-05-10 10:29:26 +1000

    fix bad output

--- iprsalt2anvio.sh
+++ iprsalt2anvio.sh
@@ -3,7 +3,7 @@
 # This script parses the output from IPRS with the -goterms option so it can be imported into anvio with anvi-import-functions
 
 INPUT=${1?I need a file to work...}
-OUTPUT=${2:-iprs_go_matrix.tsv}
+OUTPUT=${2:-iprs_ontologies_matrix.tsv}
 
 # For GO
 cut -f 1,9,14 ${INPUT} | grep "GO:" |\
@@ -14,12 +14,12 @@ for c in ${c1//|/ }
 	printf "$f1 $f2 $c\n"
 done
 done |\
-awk 'BEGIN {print "gene_callers_id\tsource\taccession\tfunction\te_value"} {print $1"\tGO\t"$3"\t\t"$2}' > ${OUTPUT%}
+awk 'BEGIN {print "gene_callers_id\tsource\taccession\tfunction\te_value"} {print $1 "\tGO\t" $3 "\t\t" $2}' > ${OUTPUT}
 
 # For pathways
 
 cut -f 1,9,15 ${INPUT} |\
-sed 's/: /:/g'|\
+sed -e 's/:\s/:/g'|\
 while read -r f1 f2 c1
 do
 for c in ${c1//|/ }
@@ -27,5 +27,5 @@ for c in ${c1//|/ }
 	printf "$f1 $f2 $c\n"
 done
 done |\
-sed 's/ /\t/g' | sed 's/:/\t/g' |\
-awk 'BEGIN {print $1"\t"$3"\t"$4"\t\t"$2}' >> ${OUTPUT%}
+sed -e 's/\s/\t/g' | sed 's/:/\t/g' |\
+awk '{print $1 "\t" $3 "\t" $4 "\t\t" $2}' >> ${OUTPUT}
